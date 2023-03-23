acaa3e81b4aff4101bcf04b57068f2bc89c6583b
venuvivek-cheruku <venuvivek.cheruku@outlook.com>
2023-03-23 00:50:06 +0000

    mobile commit one

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,81 +1,88 @@
 
 //! feature section slider
-// Get the slider container element
-const slider = document.querySelector('.featured-slider');
+// Get the necessary DOM elements
+const fiSlider = document.querySelector('.featured-in-slider');
+const fiSlidesContainer = fiSlider.querySelector('.featured-ins-container');
+const fiSlides = fiSlider.querySelectorAll('.featuredIn-slide');
+const fiDotsContainer = fiSlider.querySelector('.featured-in-dots-container');
+const fiDots = fiDotsContainer.querySelectorAll('.featured-in-dot');
+const fiPrevArrow = fiSlider.querySelector('.featured-in-arrow.prev');
+const fiNextArrow = fiSlider.querySelector('.featured-in-arrow.next');
 
-// Get the slider arrows and dots
-const prevArrow = document.querySelector('.arrow.prev');
-const nextArrow = document.querySelector('.arrow.next');
-const dots = document.querySelectorAll('.feature-slider-dots .dot');
+// Set up the initial state
+let fiCurrentSlide = 0;
+let isFiAutoPlay = true;
+const fiAutoPlayInterval = 5000; // Change this to adjust the auto play speed
 
-// Set the autoplay interval in milliseconds
-const autoplayInterval = 2000;
+// Show the first slide
+fiShowSlide(fiCurrentSlide);
 
-// Initialize the current slide index and autoplay interval ID
-let currentSlideIndex = 0;
-let autoplayIntervalId = null;
+// Set up the event listeners
+fiPrevArrow.addEventListener('click', () => {
+  fiShowSlide(fiCurrentSlide - 1);
+  resetAutoPlay();
+});
 
-// Function to show the current slide
-const showSlide = () => {
-  // Hide all slides
-  const slides = slider.querySelectorAll('.featured-slide');
-  slides.forEach((slide) => slide.classList.remove('active'));
+fiNextArrow.addEventListener('click', () => {
+  fiShowSlide(fiCurrentSlide + 1);
+  resetAutoPlay();
+});
 
-  // Show the current slide
-  slides[currentSlideIndex].classList.add('active');
-
-  // Update the dots
-  dots.forEach((dot, index) => {
-    if (index === currentSlideIndex) {
-      dot.classList.add('active');
-    } else {
-      dot.classList.remove('active');
-    }
-  });
-};
-
-// Function to go to the next slide
-const nextSlide = () => {
-  currentSlideIndex++;
-  if (currentSlideIndex >= slider.children.length) {
-    currentSlideIndex = 0;
+fiDotsContainer.addEventListener('click', e => {
+  if (e.target.classList.contains('dot')) {
+    const index = Array.from(fiDots).indexOf(e.target);
+    fiShowSlide(index);
+    resetAutoPlay();
   }
-  showSlide();
-};
+});
 
-// Function to go to the previous slide
-const prevSlide = () => {
-  currentSlideIndex--;
-  if (currentSlideIndex < 0) {
-    currentSlideIndex = slider.children.length - 1;
+fiSlider.addEventListener('mouseenter', () => {
+  isFiAutoPlay = false;
+});
+
+fiSlider.addEventListener('mouseleave', () => {
+  isFiAutoPlay = true;
+  resetAutoPlay();
+});
+
+// Auto play the slides
+let fiAutoPlayTimer;
+
+function autoPlay() {
+  if (!isFiAutoPlay) return;
+  fiAutoPlayTimer = setTimeout(() => {
+    fiShowSlide(fiCurrentSlide + 1);
+    autoPlay();
+  }, fiAutoPlayInterval);
+}
+
+function resetAutoPlay() {
+  clearTimeout(fiAutoPlayTimer);
+  autoPlay();
+}
+
+autoPlay();
+
+// Helper function to show a slide
+function fiShowSlide(index) {
+  // Wrap the index if it goes out of bounds
+  if (index < 0) {
+    index = fiSlides.length - 1;
+  } else if (index >= fiSlides.length) {
+    index = 0;
   }
-  showSlide();
-};
 
-// Start autoplay
-autoplayIntervalId = setInterval(nextSlide, autoplayInterval);
+  // Update the current slide index
+  fiCurrentSlide = index;
 
-// Add event listeners to the arrows and dots
-prevArrow.addEventListener('click', () => {
-  clearInterval(autoplayIntervalId);
-  prevSlide();
-  autoplayIntervalId = setInterval(nextSlide, autoplayInterval);
-});
+  // Move the fiSlides container to show the current slide
+  const slideWidth = fiSlides[0].offsetWidth;
+  fiSlidesContainer.style.transform = `translateX(-${slideWidth * index}px)`;
 
-nextArrow.addEventListener('click', () => {
-  clearInterval(autoplayIntervalId);
-  nextSlide();
-  autoplayIntervalId = setInterval(nextSlide, autoplayInterval);
-});
-
-dots.forEach((dot, index) => {
-  dot.addEventListener('click', () => {
-    clearInterval(autoplayIntervalId);
-    currentSlideIndex = index;
-    showSlide();
-    autoplayIntervalId = setInterval(nextSlide, autoplayInterval);
-  });
-});
+  // Update the active dot
+  Array.from(fiDots).forEach(dot => dot.classList.remove('active'));
+  fiDots[index].classList.add('active');
+}
 
 
 
@@ -92,7 +99,8 @@ let autoScroll = true;
 let mouseDown = false;
 let startX;
 let scrollLeft;
-let activeIndex = Math.floor(testimonials.length / 2);
+let activeIndex = 0;
+// let activeIndex = Math.floor(testimonials.length / 2);
 
 // Set initial active testimonial and add click event listener
 testimonials.forEach((testimonial, index) => {
@@ -103,11 +111,13 @@ testimonials.forEach((testimonial, index) => {
   testimonial.addEventListener('click', () => {
     testimonials.forEach(t => {
       t.classList.remove('active');
+      autoScroll = false;
     });
     testimonial.classList.add('active');
     autoScroll = false;
   });
   
+
   
   // Add hover event listener to stop autoplay
   testimonial.addEventListener('mouseover', () => {
@@ -119,7 +129,7 @@ testimonials.forEach((testimonial, index) => {
     mouseDown = true;
     startX = e.pageX - testimonial.offsetLeft;
     scrollLeft = testimonial.scrollLeft;
-    testimonial.classList.add('active');
+    // testimonial.classList.add('active');
   });
 
   // Add mouse up and mouse leave event listeners for dragging
@@ -243,6 +253,47 @@ document.querySelector('.right-arrow').addEventListener('click', () => {
   navigateSlider(1);
 });
 
+// !our stars sliders mobile
+const mobileImagesWrapper = document.querySelector('.our-stars-images.mobile-stars');
+const mobileImages = mobileImagesWrapper.querySelectorAll('.our-stars-image-wrapper.mobile-stars');
+const mobileContents = document.querySelectorAll('.our-stars-content.mobile-stars');
+const mobileNumImages = mobileImages.length;
+const mobileNumContents = mobileContents.length;
+let mobileActiveIndex = 0;
+
+function mobileNavigateSlider(direction) {
+  // Calculate the new active index based on the direction (1 for right, -1 for left)
+  mobileActiveIndex = (mobileActiveIndex + direction + mobileNumImages) % mobileNumImages;
+
+  // Calculate the new translateX value based on the active index
+  const translateX = 180 * mobileActiveIndex;
+
+  // Set the display and active classes for each image and content element
+  mobileImages.forEach((image, i) => {
+    const content = mobileContents[i % mobileNumContents];
+   
+    if (i === mobileActiveIndex) {
+      content.classList.add('active');
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+      content.classList.remove('active');
+    }
+
+    // Apply the new translateX value as a transform
+    image.style.transform = `translateX(${translateX}px)`;
+  });
+}
+
+// Attach click event handlers to left and right arrows
+document.querySelector('.mobile-left-arrow').addEventListener('click', () => {
+  mobileNavigateSlider(-1);
+});
+
+document.querySelector('.mobile-right-arrow').addEventListener('click', () => {
+  mobileNavigateSlider(1);
+});
+
 
 // !GSAP animations
 
@@ -269,3 +320,13 @@ reavealTL.to(".reveal-item .char", {
 });
 
 
+//!  hamburger button
+const mobileNavContainer = document.querySelector(".mobile-menu-container");
+const hamburgerButton = document.querySelector("#humburger-menu");
+
+// Add a click event listener to the hamburger button
+hamburgerButton.addEventListener("click", function () {
+  // Toggle the "open" class on the mobile-nav-container
+  mobileNavContainer.classList.toggle("open");
+  hamburgerButton.classList.toggle("open");
+});
